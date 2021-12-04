a572da37511fe0ed7e213a60c68c76758044bd3a
JFitzGerald (Manjaro) <iconium9000@gmail.com>
2021-12-04 14:24:47 -0800

    fixed refactor issues in main

--- compiler/main.py
+++ compiler/main.py
@@ -1,21 +1,23 @@
 # main.py
 import sys
-import cylexer
+import cylexer as lex
 import cyparser
-import cyparsefuns
+import cyparsefuns as funs
+import cycompiler as comp
+
 def test():
     p = cyparser.parser('test.py', 
         "a - b / c ** d // h is not -g"# - 'test' 'ing'"
         "% i != j == k > 17 > 3 and l or m or not not n")
-    p.nexttok(cylexer.tabtok)
-    n = p.rule(cyparsefuns.disjunction_r)
-    c = compile(n.itc)
+    p.nexttok(lex.tabtok)
+    n = p.rule(funs.disjunction_r)
+    c = comp.compile(n.itc)
 
 def main(filename:str):
     with open(filename, 'r') as f:
         file = f.read()
     p = cyparser.parser(filename, file)
-    r = cyparsefuns.file_r(p)
+    r = funs.file_r(p)
 
 if __name__ == "__main__":
     filename = sys.argv[1]
