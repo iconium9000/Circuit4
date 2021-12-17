2de7af874e4e0a105cfef00d54a130210a04036a
JFitzGerald <jfitzgerald@zoll.com>
2021-12-17 12:09:31 -0800

    clear cytree and cycompiler

--- compiler/cytree.py
+++ compiler/cytree.py
@@ -1,38 +1,32 @@
 # cytree.py
 from dataclasses import dataclass
 import cylexer
-from cycompiler import context, context_paths, register
+from cycompiler import context, context_paths
 
 class tree_node:
-    def asm(self, ctx:context) -> context_paths:
+
+    def asm(self, c: context) -> context_paths:
         name = self.__class__.__name__
-        msg = f"asm is not implemented for '{name}'"
+        msg = f"asm not implemented for '{name}'"
         raise NotImplementedError(msg)
+
 
 @dataclass
 class tree_range_n(tree_node):
     node:tree_node
     start_tok:cylexer.lextok
     next_tok:cylexer.lextok
-    
-    def asm(self, ctx: context) -> context_paths:
-        ctx = ctx.setrange(self.start_tok.tidx, self.next_tok.tidx)
-        return self.node.asm(ctx)
+
 
 @dataclass
 class program_n(tree_node):
     stmt:tree_node
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths = self.stmt.asm(ctx.newframe())
-        return ctx.program(paths)
+
 
 @dataclass
 class int_lit_n(tree_node):
     num:str
 
-    def asm(self, ctx: context) -> context_paths:
-        return ctx.lit_int(self.num)
 
 @dataclass
 class string_n(tree_node):
@@ -41,9 +35,7 @@ class string_n(tree_node):
 @dataclass
 class idf_n(tree_node):
     name:str
-    
-    def asm(self, ctx: context) -> context_paths:
-        return ctx.lookup_idf(self.name)
+
 
 @dataclass
 class bool_n(tree_node):
@@ -56,13 +48,7 @@ class ellipsis_n(tree_node):
 @dataclass
 class statements_n(tree_node):
     exprs:tuple[tree_node, ...]
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths = context_paths()
-        for expr in self.exprs:
-            paths, ctx = expr.asm(ctx).exclude_nxt(paths)
-            ctx = ctx.clr_tail_val()
-        return paths.join_nxt(ctx)
+
 
 @dataclass
 class raise_n(tree_node):
@@ -71,25 +57,13 @@ class raise_n(tree_node):
 @dataclass
 class yield_n(tree_node):
     expr:tree_node
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.expr.asm(ctx).exclude_nxt()
-        ctx, expr = ctx.tail_val()
-        paths = ctx.yield_expr(expr).join(paths)
-        return paths.del_vals(expr)
+
 
 @dataclass
 class hint_n(tree_node):
     trgt:tree_node
     hint:tree_node
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.trgt.asm(ctx).exclude_nxt()
-        ctx, ref = ctx.tail_val()
-        paths, ctx = self.hint.asm(ctx).exclude_nxt(paths)
-        ctx, hint = ctx.tail_val()
-        paths = ctx.hint(ref, hint).join(paths)
-        return paths.del_vals(ref, hint)
+
 
 @dataclass
 class kwarg_n(tree_node):
@@ -107,51 +81,25 @@ class kw_star_n(tree_node):
 @dataclass
 class arguments_n(tree_node):
     exprs:tuple[tree_node, ...]
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths = context_paths()
-        values:list[register] = []
-        for expr in self.exprs:
-            paths, ctx = expr.asm(ctx).exclude_nxt(paths)
-            ctx, arg = ctx.tail_val()
-            values.append(arg)
-        paths = ctx.group('args', *values).join(paths)
-        return paths.del_vals(*values)
+
 
 @dataclass
 class call_n(tree_node):
     func:tree_node
     args:tree_node
 
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.func.asm(ctx).exclude_nxt()
-        ctx, func = ctx.tail_val()
-        paths, ctx = self.args.asm(ctx).exclude_nxt(paths)
-        ctx, args = ctx.tail_val()
-        paths = ctx.call(func, args).join(paths)
-        return paths.del_vals(func, args)
 
 @dataclass
 class attribute_ref_n(tree_node):
     prim:tree_node
     attrib:str
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.prim.asm(ctx).exclude_nxt()
-        ctx, prim = ctx.tail_val()
-        paths = ctx.attrib(prim, self.attrib).join(paths)
-        return paths.del_vals(prim)
+
 
 @dataclass
 class attribute_trgt_n(tree_node):
     prim:tree_node
     attrib:str
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.prim.asm(ctx).exclude_nxt()
-        ctx, prim = ctx.tail_val()
-        paths = ctx.attrib_ref(prim, self.attrib).join(paths)
-        return paths.del_vals(prim)
+
 
 @dataclass
 class slice_n(tree_node):
@@ -163,36 +111,18 @@ class slice_n(tree_node):
 class subscript_n(tree_node):
     prim:tree_node
     arg:tree_node
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.prim.asm(ctx).exclude_nxt()
-        ctx, prim = ctx.tail_val()
-        paths, ctx = self.prim.asm(ctx).exclude_nxt(paths)
-        ctx, subscript = ctx.tail_val()
-        paths = ctx.subscript(prim, subscript).join(paths)
-        return paths.del_vals(prim, subscript)
+
 
 @dataclass
 class subscript_trgt_n(tree_node):
     prim:tree_node
     arg:tree_node
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.prim.asm(ctx).exclude_nxt()
-        ctx, prim = ctx.tail_val()
-        paths, ctx = self.prim.asm(ctx).exclude_nxt(paths)
-        ctx, subscript = ctx.tail_val()
-        paths = ctx.subscript_ref(prim, subscript).join(paths)
-        return paths.del_vals(prim, subscript)
+
 
 @dataclass
 class idf_trgt_n(tree_node):
     name:str
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = ctx.lookup_idf(self.name).exclude_nxt()
-        ctx, trgt = ctx.tail_val()
-        return ctx.lookup_ref(trgt).join(paths).del_vals(trgt)
+
 
 @dataclass
 class star_trgt_n(tree_node):
@@ -205,16 +135,7 @@ class tuple_trgt_n(tree_node):
 @dataclass
 class tuple_n(tree_node):
     exprs:tuple[tree_node, ...]
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths = context_paths()
-        values:list[register] = []
-        for expr in self.exprs:
-            paths, ctx = expr.asm(ctx).exclude_nxt(paths)
-            ctx, tupval = ctx.tail_val()
-            values.append(tupval)
-        paths = ctx.group('tuple', *values).join(paths)
-        return paths.del_vals(*values)
+
 
 @dataclass
 class list_trgt_n(tree_node):
@@ -223,16 +144,7 @@ class list_trgt_n(tree_node):
 @dataclass
 class list_n(tree_node):
     exprs:tuple[tree_node, ...]
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths = context_paths()
-        values:list[register] = []
-        for expr in self.exprs:
-            paths, ctx = expr.asm(ctx).exclude_nxt(paths)
-            ctx, listval = ctx.tail_val()
-            values.append(listval)
-        paths = ctx.group('list', *values).join(paths)
-        return paths.del_vals(*values)
+
 
 @dataclass
 class iter_n(tree_node):
@@ -244,24 +156,7 @@ class for_n(tree_node):
     trgt:tree_node
     iterable:tree_node
     block:tree_node
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.iterable.asm(ctx).exclude_nxt()
-        ctx, iterable = ctx.tail_val()
-        tpaths = ctx.iterator(iterable).del_vals(iterable)
-        paths, ctx = tpaths.exclude_nxt(paths)
-        ctx, iterator = ctx.tail_val()
 
-        fctx = ctx.newloop()
-        fpaths, fctx = fctx.next_element(iterator).exclude_nxt()
-        fctx, next_element = fctx.tail_val()
-        fpaths, fctx = self.trgt.asm(fctx).exclude_nxt(fpaths)
-        fctx, trgt = fctx.tail_val()
-        tpaths = fctx.assign(next_element, trgt).del_vals(next_element, trgt)
-        fpaths, fctx = tpaths.exclude_nxt(fpaths)
-        fpaths = self.block.asm(fctx).join(fpaths)
-
-        return ctx.loop(fpaths).join(paths).del_vals(iterator)
 
 @dataclass
 class if_n(tree_node):
@@ -269,18 +164,11 @@ class if_n(tree_node):
     true_block:tree_node
     false_block:tree_node
 
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.test.asm(ctx).exclude_nxt()
-        paths, ctx_true, ctx_false = ctx.branch(paths)
-        true_paths = self.true_block.asm(ctx_true)
-        false_paths = self.false_block.asm(ctx_false)
-        return paths.join(true_paths, false_paths)
 
 @dataclass
 class pass_n(tree_node):
-    
-    def asm(self, ctx: context) -> context_paths:
-        return context_paths().join_nxt(ctx.clr_tail_val())
+    pass
+
 
 @dataclass
 class continue_n(tree_node):
@@ -293,10 +181,7 @@ class async_n(tree_node):
 @dataclass
 class generator_n(tree_node):
     stmt:tree_node
-    
-    def asm(self, ctx: context) -> context_paths:
-        generator_paths = self.stmt.asm(ctx.newframe())
-        return ctx.generator(generator_paths)
+
 
 @dataclass
 class or_block_n(tree_node):
@@ -308,13 +193,6 @@ class or_block_n(tree_node):
 
     exprs:'tuple[tree_node, ...]'
 
-    def asm(self, ctx: context) -> context_paths:
-        paths = context_paths()
-        for expr in self.exprs:
-            paths, ctx = expr.asm(ctx).exclude_nxt(paths)
-            paths, if_true, ctx = ctx.branch(paths)
-            paths = paths.join_nxt(if_true)
-        return paths.join_nxt(ctx)
 
 @dataclass
 class and_block_n(tree_node):
@@ -326,45 +204,18 @@ class and_block_n(tree_node):
 
     exprs:'tuple[tree_node, ...]'
 
-    def asm(self, ctx: context) -> context_paths:
-        paths = context_paths()
-        for expr in self.exprs:
-            paths, ctx = expr.asm(ctx).exclude_nxt(paths)
-            paths, ctx, if_false = ctx.branch(paths)
-            paths = paths.join_nxt(if_false)
-        return paths.join_nxt(ctx)
 
 @dataclass
 class assignment_n(tree_node):
     expr:tree_node
     trgts:tuple[tree_node, ...]
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.expr.asm(ctx).exclude_nxt()
-        ctx, expr = ctx.tail_val()
-        for trgt in self.trgts:
-            paths, ctx = trgt.asm(ctx).exclude_nxt(paths)
-            ctx, trgt = ctx.tail_val()
-            paths, ctx = ctx.assign(trgt, expr).del_vals(trgt).exclude_nxt(paths)
-        return paths.join_nxt(ctx).del_vals(expr)
+
 
 @dataclass
 class compare_n(tree_node):
     expr:tree_node
     compares:tuple[tuple[str,tree_node], ...]
- 
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.expr.asm(ctx).exclude_nxt()
-        ctx, arga = ctx.tail_val()
-        for op, expr in self.compares:
-            paths, ctx = expr.asm(ctx).exclude_nxt(paths)
-            ctx, argb = ctx.tail_val()
-            tpaths = ctx.binop(op, arga, argb).del_vals(arga)
-            arga = argb
-            paths, ctx = tpaths.exclude_nxt(paths)
-            paths, ctx, if_false = ctx.branch(paths)
-            paths = paths.join_nxt(if_false)
-        return paths.join_nxt(ctx).del_vals(arga)
+
 
 @dataclass
 class binary_op_n(tree_node):
@@ -374,14 +225,7 @@ class binary_op_n(tree_node):
     op:str
     expr_a:tree_node
     expr_b:tree_node
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.expr_a.asm(ctx).exclude_nxt()
-        ctx, arga = ctx.tail_val()
-        paths, ctx = self.expr_b.asm(ctx).exclude_nxt(paths)
-        ctx, argb = ctx.tail_val()
-        paths = ctx.binop(self.op, arga, argb).join(paths)
-        return paths.del_vals(arga, argb)
+
 
 @dataclass
 class unary_op_n(tree_node):
@@ -390,12 +234,7 @@ class unary_op_n(tree_node):
     '''
     op:str
     expr:tree_node
-    
-    def asm(self, ctx: context) -> context_paths:
-        paths, ctx = self.expr.asm(ctx).exclude_nxt()
-        ctx, arg = ctx.tail_val()
-        paths = ctx.unop(self.op, arg).join(paths)
-        return paths.del_vals(arg)
+
 
 @dataclass
 class await_n(tree_node):
